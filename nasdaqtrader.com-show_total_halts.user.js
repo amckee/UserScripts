c10c5ff50955027cd7bd861658daa65b2bdd9845
Adam McKee <armware@gmail.com>
2024-08-05 12:06:42 -0500

    Move output

--- nasdaqtrader.com-show_total_halts.user.js
+++ nasdaqtrader.com-show_total_halts.user.js
@@ -8,6 +8,7 @@
 // @updateURL   https://raw.githubusercontent.com/amckee/UserScripts/main/nasdaqtrader.com-show_total_halts.user.js
 // @downloadURL https://raw.githubusercontent.com/amckee/UserScripts/main/nasdaqtrader.com-show_total_halts.user.js
 // @supportURL  https://github.com/amckee/UserScripts/issues
+// @run-at      document-end
 // ==/UserScript==
 
 (function() {
@@ -17,22 +18,25 @@
         // load entire table
         var haltsTable = document.querySelector("#divTradeHaltResults > div > table");
 
+        console.log("haltsTable: " + haltsTable);
+
         // filter halts for todays halts only and count 'em
         var today = new Date();
         var todayDate = today.toLocaleDateString();
-        var totalHalts = 0;
+        console.log("todayDate: " + todayDate);
+        var todaysHalts = 0;
 
         for (var i = 1; i < haltsTable.rows.length; i++) {
             var haltDate = haltsTable.rows[i].cells[0].textContent.trim();
             if (haltDate === todayDate) {
-                totalHalts++;
+                todaysHalts++;
             }
         }
 
-        // display the total number of halted trades
-        var totalHaltsElement = document.createElement("div");
-        totalHaltsElement.textContent = "Total Halts Today: " + totalHalts;
-        document.body.appendChild(totalHaltsElement);
+
+        // Add the count to the main page header
+        var pageHeader = document.querySelector("#rightWideCOL > h1");
+        pageHeader.textContent = todaysHalts + " " + pageHeader.textContent;
     }
 
     setInterval(showTotalHalts, 1000);
